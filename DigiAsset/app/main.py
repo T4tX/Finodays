cc2f1d5cf4d61ad55352aa850ba8a110140514be
Solo_Way <61106206+T4tX@users.noreply.github.com>
2023-08-28 17:43:15 +0300

    Add files via upload

---
+++ DigiAsset/app/main.py
@@ -0,0 +1,65 @@
+from typing import Union, List
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+class Products(BaseModel):
+    name: str
+    token: str
+    #is_sale: Union[bool, None] = None
+    category: str
+
+class Order(BaseModel):
+    product_id: int
+    price: int
+    payment_method: int
+
+@app.get("/")
+async def index():
+    return {'hello':'world'}
+'''@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put(("/items/{item_id}"))
+async def put_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}'''
+
+@app.get(("/products"))
+async def get_products_list(category: Union[str, None] = None):
+    return {'products':'list'}
+
+@app.get(("/products/{id}"))
+async def get_products_by_id(id: int):
+    return {'product_id': id,'product_name': 'name'}
+
+@app.get(("/orders/{user_id}"))
+async def get_user_orders_by_id(user_id: int):
+    return {'user_id': user_id, 'orders': 'orders'}
+
+@app.post(("/orders"))
+async def make_order(order_list: List[Order]):
+    return {"Confirmed"}
+
+@app.get(("/orders/{id}"))
+async def get_order_by_id(id: int):
+    return {'order_id': id, 'order_status': 'status'}
+
+@app.put(("/orders/{id}/cancel"))
+async def order_cansel(id: int):
+    return {"order_id": id, "order_status": "canseled"}
+
+@app.put(("/orders/{id}/confirm_payment"))
+async def order_confirm(id: int):
+    return {"order_id": id, "order_status": "confirmed"}
+
+'''@app.get(("/products"))
+async def get_products_list(oproduct_list: List[Products], category: Union[str, None] = None):
+    return oproduct_list'''
---
+++ DigiAsset/database/db.py
@@ -0,0 +1,44 @@
+import psycopg2
+
+
+conn = psycopg2.connect(dbname='postgres', user='postgres',
+
+                        password='postgres', host='db')
+
+cursor = conn.cursor()
+
+
+def db_init():
+
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS users (id serial NOT NULL PRIMARY KEY,login TEXT, password TEXT, reg_date TEXT)")
+
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS products (id serial NOT NULL PRIMARY KEY,user_id INTEGER, token TEXT NOT NULL, name TEXT NOT NULL, create_date TEXT)")
+
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS orders (id serial NOT NULL PRIMARY KEY,from_user_id INTEGER,to_user_id INTEGER, price INTEGER NOT NULL, product_id INTEGER NOT NULL, create_date TEXT)")
+
+    cursor.execute(
+        "insert into users values (default,'test1','test1','01.10.2022'),(default,'test2','test2','01.10.2022')")
+    cursor.execute(
+        "insert into products values (default,0,'token1','name1','01.10.2022'),(default,0,'token2','name2','01.10.2022')")
+    cursor.execute(
+        "insert into orders values (default,0,1,100,0,'01.10.2022'),(default,1,0,100,1,'01.10.2022')")
+    cursor.execute('select * from users')
+
+    records = cursor.fetchall()
+
+    print(records)
+
+
+db_init()
+
+
+def query(query):
+
+    cursor.execute(query)
+
+    records = cursor.fetchall()
+
+    return records
