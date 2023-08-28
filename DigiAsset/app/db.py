5da0e2e43ba3db54a3504e0dbe5c59a64187b04e
Solo_Way <61106206+T4tX@users.noreply.github.com>
2023-08-29 00:09:05 +0300

    Add files via upload

--- DigiAsset/app/db.py
+++ DigiAsset/app/db.py
@@ -9,7 +9,11 @@ cursor = conn.cursor()
 
 
 def db_init():
+    """
+    Инициализация базы данных.
 
+    Создает таблицы users, products, orders и заполняет их тестовыми данными.
+    """
     cursor.execute(
         "CREATE TABLE IF NOT EXISTS users (id serial NOT NULL PRIMARY KEY,login TEXT, password TEXT, reg_date TEXT)")
 
@@ -36,12 +40,15 @@ db_init()
 
 
 def query(query):
+    """
+    Выполняет SQL-запрос и возвращает результат в виде списка кортежей.
+
+    :param query: SQL-запрос
+    :return: список кортежей с результатами запроса или None, если запрос не удалось выполнить
+    """
     cursor.execute(query)
     try:
         records = cursor.fetchall()
         return records
     except:
         return None
-
-
-
